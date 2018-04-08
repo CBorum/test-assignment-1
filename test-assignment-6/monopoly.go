6e687f178c171a4420567575d260a22a96aa071a
CBorum <christopherborum@gmail.com>
2018-04-08 15:57:11 +0200

    monopoly

--- test-assignment-6/monopoly.go
+++ test-assignment-6/monopoly.go
@@ -1,6 +1,7 @@
 package monopoly
 
 import (
+	"github.com/stretchr/testify/mock"
 	"math/rand"
 	"time"
 )
@@ -31,7 +32,11 @@ type board struct {
 	squares int
 }
 
-type dice struct{
+type monopolyDice interface {
+	roll() int
+}
+
+type dice struct {
 	seed int64
 }
 
@@ -41,7 +46,7 @@ func (p *player) getPiece() *piece {
 
 func move(piece monopolyPiece, diceVal int) {
 	newLoc := piece.getLocation() + diceVal // b) Calculating the new square location
-	piece.setLocation(newLoc) // c) Moving the player’s piece from the old location to the new square location
+	piece.setLocation(newLoc)               // c) Moving the player’s piece from the old location to the new square location
 }
 
 func (p *piece) getLocation() int {
@@ -52,7 +57,7 @@ func (p *piece) setLocation(newLoc int) {
 	p.loc = newLoc
 }
 
-func takeTurn(p monopolyPlayer, d *dice) {
+func takeTurn(p monopolyPlayer, d monopolyDice) {
 	diceVal := d.roll()
 	diceVal += d.roll() // a) Calculating a random number total between 2 and 12 (the range of two dice)
 	println("dice value:", diceVal)
@@ -64,8 +69,28 @@ func takeTurn(p monopolyPlayer, d *dice) {
 
 func (d *dice) roll() int {
 	if d.seed == 0 {
-		println("seed")
 		rand.Seed(time.Now().Unix())
 	}
 	return 1 + rand.Intn(6)
+}
+
+// mock
+
+type mockedDice struct {
+	mock.Mock
+}
+
+func (d *mockedDice) roll() int {
+	args := d.Called()
+	return args.Int(0)
+}
+
+type mockedPlayer struct {
+	mock.Mock
+	piece *piece
+}
+
+func (p *mockedPlayer) getPiece() *piece {
+	args := p.Called()
+	return args.Get(0).(*piece)
 }
--- test-assignment-6/monopoly_test.go
+++ test-assignment-6/monopoly_test.go
@@ -16,6 +16,14 @@ func TestRoll(t *testing.T) {
 	}
 }
 
+func TestMockRoll(t *testing.T) {
+	d := &mockedDice{}
+	d.On("roll").Return(123)
+	d.roll()
+	d.roll()
+	d.AssertNumberOfCalls(t, "roll", 2)
+}
+
 func TestTakeTurn(t *testing.T) {
 	p := &player{
 		piece: &piece{
@@ -26,4 +34,31 @@ func TestTakeTurn(t *testing.T) {
 		seed: 1, // Using "fakes", same seed will always produce the same values
 	}
 	takeTurn(p, d)
+}
+
+func TestTakeTurn2(t *testing.T) {
+	p := &player{
+		piece: &piece{
+			loc: 1,
+		},
+	}
+	d := &mockedDice{}
+	// mocked responses -> piece location will always be the same
+	d.On("roll").Return(2).Once()
+	d.On("roll").Return(4).Once()
+	takeTurn(p, d)
+	loc := p.getPiece().getLocation()
+	assert.Equal(t, 7, loc)
+}
+
+func TestTakeTurn3(t *testing.T) {
+	p := &mockedPlayer{}
+	p.On("getPiece").Return(&piece{123})
+	d := &mockedDice{}
+	// mocked responses -> piece location will always be the same
+	d.On("roll").Return(2).Once()
+	d.On("roll").Return(4).Once()
+	takeTurn(p, d)
+	loc := p.getPiece().getLocation()
+	assert.Equal(t, 129, loc)
 }
